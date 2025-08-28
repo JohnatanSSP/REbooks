d89d6cdf5bbbaa45cda578960f33e0bb42e24eb5
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2025-08-28 14:14:52 -0300

    criar uma entidade a partir do ninja model

---
+++ src/main/java/johnatanSSP/REbooks/BooksModel.java
@@ -0,0 +1,80 @@
+package johnatanSSP.REbooks;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_register")
+public class BooksModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
+    private String Title;
+    private String Author;
+    private String ReleaseDate;
+    private String Genre;
+    private int Pages;
+    private String Reader;
+
+
+    public void BooksModel(){
+
+    }
+
+    public BooksModel(String title, String author, String releaseDate, String genre, int pages, String reader) {
+        Title = title;
+        Author = author;
+        ReleaseDate = releaseDate;
+        Genre = genre;
+        Pages = pages;
+        Reader = reader;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+
+    public String getReleaseDate() {
+        return ReleaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        ReleaseDate = releaseDate;
+    }
+
+    public String getGenre() {
+        return Genre;
+    }
+
+    public void setGenre(String genre) {
+        Genre = genre;
+    }
+
+    public int getPages() {
+        return Pages;
+    }
+
+    public void setPages(int pages) {
+        Pages = pages;
+    }
+
+    public String getReader() {
+        return Reader;
+    }
+
+    public void setReader(String reader) {
+        Reader = reader;
+    }
+}
