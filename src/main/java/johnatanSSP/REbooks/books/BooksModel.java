943070654ef2678d2549c353b5ff2826e498a599
johnatanssp <johnatan.sousa@gmail.com>
2025-09-01 14:43:34 -0300

    feat: remove constructors, and add anotations @Noargs, @AllArgs, @Data

--- src/main/java/johnatanSSP/REbooks/books/BooksModel.java
+++ src/main/java/johnatanSSP/REbooks/books/BooksModel.java
@@ -2,11 +2,17 @@ package johnatanSSP.REbooks.books;
 
 import jakarta.persistence.*;
 import johnatanSSP.REbooks.archives.ArchivesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_register")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class BooksModel {
 
     @Id
@@ -22,63 +28,4 @@ public class BooksModel {
     @JoinColumn(name = "archive_id")
     private ArchivesModel archive;
 
-    public BooksModel() {
-    }
-
-    public BooksModel(String title, String author, String releaseDate, String genre, int pages, String reader) {
-        Title = title;
-        Author = author;
-        ReleaseDate = releaseDate;
-        Genre = genre;
-        Pages = pages;
-        Reader = reader;
-    }
-
-    public String getTitle() {
-        return Title;
-    }
-
-    public void setTitle(String title) {
-        Title = title;
-    }
-
-    public String getAuthor() {
-        return Author;
-    }
-
-    public void setAuthor(String author) {
-        Author = author;
-    }
-
-    public String getReleaseDate() {
-        return ReleaseDate;
-    }
-
-    public void setReleaseDate(String releaseDate) {
-        ReleaseDate = releaseDate;
-    }
-
-    public String getGenre() {
-        return Genre;
-    }
-
-    public void setGenre(String genre) {
-        Genre = genre;
-    }
-
-    public int getPages() {
-        return Pages;
-    }
-
-    public void setPages(int pages) {
-        Pages = pages;
-    }
-
-    public String getReader() {
-        return Reader;
-    }
-
-    public void setReader(String reader) {
-        Reader = reader;
-    }
 }
