f40def5a067b420107d003fe92d48c15f041f9e5
johnatanssp <johnatan.sousa@gmail.com>
2025-09-05 17:42:11 -0300

    chore: include DTO and Mapper in entity

--- src/main/java/johnatanSSP/REbooks/books/BookDTO.java
+++ src/main/java/johnatanSSP/REbooks/books/BookDTO.java
@@ -1,4 +1,21 @@
 package johnatanSSP.REbooks.books;
+import johnatanSSP.REbooks.archives.ArchivesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class BookDTO {
+
+    private long Id;
+    private String Title;
+    private String Author;
+    private String ReleaseDate;
+    private String Genre;
+    private int Pages;
+    private String Reader;
+    private ArchivesModel archive;
+    private String review;
 }
