4a71c3fe1c3ba8328b5f832372d0336b057737c5
johnatanssp <johnatan.sousa@gmail.com>
2025-09-09 18:30:42 -0300

    feat: add routes for navigation

--- src/main/java/johnatanSSP/REbooks/books/BookControllerUI.java
+++ src/main/java/johnatanSSP/REbooks/books/BookControllerUI.java
@@ -1,9 +1,11 @@
 package johnatanSSP.REbooks.books;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -17,10 +19,41 @@ public class BookControllerUI {
         this.booksService = booksService;
     }
 
-    @GetMapping("/show-all")
+    @GetMapping("/showall")
     public String showAll(Model model) {
         List<BookDTO> book = booksService.showAll();
         model.addAttribute("books", book);
-        return "showAll";
+        return "showall";
     }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id){
+        booksService.deleteBookById(id);
+        return "redirect:/books/ui/show-all";
+    }
+
+    @GetMapping("/show/{id}")
+    public String showAllID(@PathVariable Long id, Model model){
+        BookDTO book = booksService.findById(id);
+        if(book!=null){
+            model.addAttribute("book", book);
+            return "detailsbook";
+        }else {
+            model.addAttribute("mensagem", "book not found");
+            return "showall";
+        }
+    }
+
+//    @GetMapping("/add")
+//    public String addBook(Model model){
+//        model.addAttribute("book", new BookDTO());
+//        return "addbook";
+//    }
+//
+//    @PostMapping("/save")
+//    public String saveBook(@ModelAttribute BookDTO book, RedirectAttributes redirectAttributes){
+//        booksService.createBook(book);
+//        redirectAttributes.addFlashAttribute("mensagem", "Book saved successfully");
+//        return "redirect:/books/ui/showall";
+//    }
 }
